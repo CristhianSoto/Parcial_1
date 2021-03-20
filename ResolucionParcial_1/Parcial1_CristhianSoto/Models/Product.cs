f51af57bc8bb2a089aff8b355833afd3d5c5ce86
CristhianSoto <a2018210980@estudiantes.upsa.edu.bo>
2021-03-20 11:04:38 -0400

    modelo incorporado

---
+++ ResolucionParcial_1/Parcial1_CristhianSoto/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Parcial1_CristhianSoto.Models
+{
+    public class Product
+    {
+        [Key]
+        [Range(1,99999)]
+        public int ProductId { get; set; }
+        [Required]
+        [StringLength(30, MinimumLength = 3, ErrorMessage = "Ingresar nombres entre 3 a 30 caracteres")]
+        public string Description { get; set; }
+        [Required]
+        [DataType(DataType.Currency)]
+        public int Price { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime LastBuy { get; set; }
+    }
+}
